8b9b67f94b0828453188c4c2c14fae3684a64e19
aboomar13 <aboomar5464@gmail.com>
2026-06-17 03:54:45 -0700

    js folder with script file created

---
+++ js/script.js
@@ -0,0 +1,222 @@
+// STEP 1
+
+// Convert identifiers to Camel Case notation
+
+// let some_month      -> let someMonth
+// function the Month() -> function theMonth()
+// let current-month   -> let currentMonth
+// let summer_month    -> let summerMonth
+// let MyLibrary-function -> let myLibraryFunction
+
+
+
+// STEP 2
+
+// Literal expressions
+
+// Numeric Literal
+// console.log(100);
+
+// String Literal
+// console.log("Hello World");
+
+// Boolean Literal
+// console.log(true);
+
+// Null Literal
+// console.log(null);
+
+
+
+// STEP 3
+
+// Complex / Variable Expressions
+
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// let firstName = "John";
+// let lastName = "Smith";
+// console.log(firstName + " " + lastName);
+
+
+
+// STEP 4
+
+// Declare 9 variables using Camel Case and Hungarian Notation
+
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let intZipCode;
+// let intAge;
+// let strReferralSource;
+// let blnMayWeContactYou;
+
+
+
+// STEP 5
+
+// Three ways of declaring and assigning values
+
+// let strFirstName = "Eias";
+
+// let strLastName;
+// strLastName = "Alhomsi";
+
+// let strCity = "San Diego", strState = "California"
+
+
+
+// STEP 6
+
+// Create a variable
+// let myNumber = 13;
+
+// Add a number and a string 
+// console.log(myNumber + ' ' + 'is my favorite number.')
+
+
+
+// STEP 7
+
+// Boolean + String
+// let result1 = true + " is fact";
+// console.log(result1);
+
+// Number + Boolean
+// let result2 = 12 + true;
+// console.log(result2); 
+
+
+
+// STEP 8
+
+// Valid string literal
+
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString);
+
+
+
+// STEP 9
+
+// Null value
+// let firstValue = null;
+// console.log(firstValue);
+
+// Undefined value
+// let secondValue;
+// console.log(secondValue);
+
+
+
+// STEP 10
+
+// typeof operator
+
+// console.log(typeof "Hello");     // string
+// console.log(typeof 100);         // number
+// console.log(typeof true);        // boolean
+// console.log(typeof null);        // object
+// console.log(typeof undefined);   // undefined
+
+
+
+// STEP 11
+
+// Alert with concatenation
+
+// alert("Hello " + "Eias Alhomsi" + ", welcome to the JavaScript class!");
+
+
+
+// STEP 12
+
+// Variable name
+
+// let name = "Eias Alhomsi";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+
+
+// STEP 13
+
+// Variable course
+
+// let name = "Eias Alhomsi";
+// let course = "JavaScript";
+// alert("Hello " + name + ", welcome to the " + course + " class!");
+
+
+
+// STEP 14
+
+// Line break
+
+// let name = "Eias Alhomsi";
+// let course = "JavaScript";
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+
+
+// STEP 15
+
+// Prompt for name
+
+// let name = prompt("What is your name?");
+// let course = "JavaScript";
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+
+
+// STEP 16
+
+//Prompt for course
+
+// let name = prompt('What is your name?');
+// let course = prompt('What class are you taking?');
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+
+
+
+// STEP 17
+
+// Sum of x and y
+
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+
+
+// STEP 18
+
+// Add and assign
+
+// let x = 20;
+// x += 20;
+// console.log(x); 
+
+
+
+// STEP 19
+
+// Multiply and assign
+
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+
+
+// STEP 20
+
+// Remainder, then divide and assign
+
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
